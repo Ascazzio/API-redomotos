e95aa7e0cab8fafd1b257f85f98061f3154aaf34
Fabrizio Pironi <fabriziopironi@gmail.com>
2022-02-28 08:00:51 +0100

    mini-seed part agregado

---
+++ seeders/01-demo-parts.js
@@ -0,0 +1,34 @@
+'use strict';
+const bcrypt = require('bcrypt');
+const authConfig = require('../config/auth');
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+  
+      await queryInterface.bulkInsert('parts', [{
+        name: "Discos de freno",
+        description: "Discos de freno ventilados",
+        pictureLink: "sin foto",
+        type: "Frenos",
+        Brand: "StopTech",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Suspension deportiva",
+        description: "Amortiguadores de alto rendimiento",
+        pictureLink: "sin foto",
+        type: "Suspension",
+        Brand: "Fox",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }], {});
+    },
+  
+    down: async (queryInterface, Sequelize) => {
+  
+      await queryInterface.bulkDelete('users', null, {});
+  
+    }
+};
+  
